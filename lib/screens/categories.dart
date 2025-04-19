823c13706f0102581e3f78278b7bd86475bc3e09
Sanskar Biyani <sanskarbiyani902@gmail.com>
2025-04-19 21:01:28 +0530

    explicit animation in categories screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+
 import 'package:meals_app/data/dummy_dat.dart';
 import 'package:meals_app/models/category.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   final List<Meal> availableMeals;
 
   const CategoriesScreen({
@@ -13,8 +14,36 @@ class CategoriesScreen extends StatelessWidget {
     required this.availableMeals,
   });
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    // To dispose the animation controller and prevent memory links
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final filteredList = availableMeals
+    final filteredList = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
@@ -29,22 +58,31 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(10),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: availableCategories
+            .map((category) => CategoryGridItem(
+                  category: category,
+                  onSelectCategory: () {
+                    _selectCategory(context, category);
+                  },
+                ))
+            .toList(),
       ),
-      children: availableCategories
-          .map((category) => CategoryGridItem(
-                category: category,
-                onSelectCategory: () {
-                  _selectCategory(context, category);
-                },
-              ))
-          .toList(),
+      builder: (context, child) => Padding(
+        // The child widget is a widget mainly should be outputed as part of an animated content,
+        // but should not be animated themselves, therby increasing performance.
+        padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+        child: child,
+      ),
     );
   }
 }
